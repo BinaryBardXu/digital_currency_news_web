72efd1761fe3c9a550981e8cb766120a4ab26d54
Tao <bitao.ux@gmail.com>
2018-03-04 13:48:39 +0800

    Optimize news list style.

--- src/components/News.vue
+++ src/components/News.vue
@@ -7,30 +7,31 @@
 </template>
 
 <script>
-import axios from 'axios'
-import item from './Item'
+import axios from "axios";
+import item from "./Item";
 
-import virtualList from 'vue-virtual-scroll-list'
+import virtualList from "vue-virtual-scroll-list";
 
 export default {
-  data () {
+  data() {
     return {
       newsList: [],
       errors: []
-    }
+    };
   },
-  components: { item, 'virtual-list': virtualList },
+  components: { item, "virtual-list": virtualList },
 
   // Fetches posts when the component is created.
-  created () {
-    axios.get(`/api/v1.0/news`)
+  created() {
+    axios
+      .get(`/api/v1.0/news`)
       .then(response => {
-      // JSON responses are automatically parsed.
-        this.newsList = response.data
+        // JSON responses are automatically parsed.
+        this.newsList = response.data;
       })
       .catch(e => {
-        this.errors.push(e)
-      })
+        this.errors.push(e);
+      });
 
     // async / await version (created() becomes async created())
     //
@@ -41,16 +42,16 @@ export default {
     //   this.errors.push(e)
     // }
   }
-}
+};
 </script>
 <style>
-    .news-list {
-        height: 100%;
-            width: 50%;
-    margin: auto;
-    }
-    .list-group-item{
-      background-color: transparent !important;
-      border: none !important;
-    }
+.news-list {
+  height: 100%;
+  width: 65%;
+  margin: auto;
+}
+.list-group-item {
+  background-color: transparent !important;
+  border: none !important;
+}
 </style>
